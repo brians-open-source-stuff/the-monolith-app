f1a21724a1114c70cfc283463e2b55baa025fa9e
Brian Emilius <brian@brianemilius.com>
2019-10-04 18:41:16 +0200

    add getAllProducts function to controller

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -1,5 +1,21 @@
+const { getAllProducts } = require("../models/product.model");
+
 exports.showOptions = function(req, res) {
   res.status(204);
-  res.header("Allow", "OPTIONS");
+  res.header("Allow", "OPTIONS, GET");
   res.end();
+};
+
+exports.getAllProducts = async function(req, res, next) {
+  try {
+    let page = req.query.page ? parseInt(req.query.page) : 1;
+    let limit = req.query.limit ? parseInt(req.query.limit) : 10;
+    page = page < 1 ? 1 : page;
+    limit = limit < 1 ? 1 : limit;
+
+    const products = await getAllProducts(page, limit);
+    res.json(products);
+  } catch (error) {
+    next(error);
+  }
 };

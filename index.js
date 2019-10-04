cbbb44b42fd7ed71d9ef93a47b757325c554c36f
Brian Emilius <brian@brianemilius.com>
2019-10-04 16:40:40 +0200

    add routes autoload function

--- index.js
+++ index.js
@@ -2,5 +2,11 @@ require("dotenv").config();
 const express = require("express");
 const router = express.Router();
 const app = express();
+const { readdirSync } = require("fs");
+const { join } = require("path");
+
+readdirSync(join(__dirname, "routes")).forEach(function(file) {
+  require(join(__dirname, "routes", file))(router);
+});
 
 require("./server")(app);

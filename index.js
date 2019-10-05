c61396fa256746b17cae678bede15e84a0712e17
Brian Emilius <brian@brianemilius.com>
2019-10-05 15:15:12 +0200

    add cache to router

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const router = express.Router();
 const app = express();
 const { readdirSync } = require("fs");
 const { join } = require("path");
+const cache = require("./config/cache");
+
+router.all(cache(10));
 
 readdirSync(join(__dirname, "routes")).forEach(function(file) {
   require(join(__dirname, "routes", file))(router);
